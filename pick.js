cf8f3e675f5d3ebc5b0f831ccd87babfd2e95e1d
colecrum26 <cwcrumrine@gmail.com>
2023-09-27 15:03:13 -0500

    working on button input for grid creation

---
+++ pick.js
@@ -0,0 +1,33 @@
+const ec = document.getElementById("etch-container");
+let numBoxes;
+
+// document.getElementById("btns").addEventListener("click", function(evt) {
+//     let target = evt.target;
+//     if (target.id === "btn-small") {
+//         numBoxes = 100;
+//     } else if (target.id === "btn-medium") {
+//         numBoxes = 256;
+//     } else {
+//         numBoxes = 484;
+//     }
+// });
+
+document.getElementById("btn-small").addEventListener("click", function(e) {
+   numBoxes = 100;
+   console.log(numBoxes)
+});
+
+function sketch(numBoxes) {
+    ec.innerHTML = "";
+  
+    for (let i = 0; i < numBoxes; i++) {
+      let box = document.createElement("div");
+      box.classList.add("grid-box");
+      ec.appendChild(box);
+  
+      const color = () => {box.style.backgroundColor = "#000"};
+      box.addEventListener("click", color);
+    }
+  }
+  
+  sketch(numBoxes);
